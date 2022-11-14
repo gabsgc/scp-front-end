ffbf5fed1f935335bc592bff7acc51382b7e6601
Wallas Curty <65136877+WallasCurty@users.noreply.github.com>
2022-11-14 20:29:37 -0300

    POST pessoa

--- src/components/Tabela/index.js
+++ src/components/Tabela/index.js
@@ -14,6 +14,17 @@ export default function Tabela() {
         load();
     }, []);
 
+    async function pessoaUser(){
+        await fetch (`${baseURL}`,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify()
+        }) 
+            .then(data => data.json())
+    }
+
     async function load() {
         await fetch(`${baseURL}`, {
             method: 'GET',
