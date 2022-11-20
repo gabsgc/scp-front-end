7e201d5ca7e945562c278ad2531f9c245374c637
gabsgc <gabriellagarccia@gmail.com>
2022-11-20 19:33:56 -0300

    add logout and update styles

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Navbar() {
+    const navigate = useNavigate();
+    
+    const logout = (e) => {
+        e.preventDefault();
+        console.log('Logout');
+
+        localStorage.clear();
+
+        navigate("/");
+    }
     return (
         <>
             <nav className="navbar navbar-dark bg-primary">
-                <a className="navbar-brand bg-primary" href="/home">
-                    <span className="badge badge-warning bg-warning m-2">SCP</span>
-                    Sistema de Controle de Presença
-                </a>
+                <div className="container-fluid">
+                    <a className="navbar-brand bg-primary" href="/home">
+                        <span className="badge badge-warning bg-warning m-2">SCP</span>
+                        Sistema de Controle de Presença
+                    </a>
+                    <Link
+                        to="#"
+                        onClick={logout}
+                    >
+                    <button class="btn btn-dark" type="submit">
+                        Logout</button>
+                    </Link>
+                </div>
             </nav>
-
         </>
     )
 }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-//import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import BannerLogin from '../../assets/img/login.svg';
+import LogoIES from '../../assets/img/logo_ies.png';
+
+import './style.css'
 
 async function loginUser(credentials) {
   return fetch('http://localhost:8080/login', {
@@ -20,8 +23,8 @@ export default function Login({ setToken }) {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
 
-  //const navigate = useNavigate();
-  
+  const navigate = useNavigate();
+
   const handleSubmit = async e => {
     e.preventDefault();
     const token = await loginUser({
@@ -29,42 +32,45 @@ export default function Login({ setToken }) {
       password
     });
     setToken(token);
-    //navigate(`/home`);
+    navigate("/home");
   }
 
   return (
     <>
-      <h1 className='text-center text-primary pt-5'>Sistema de Controle de Presença</h1>
-      <div class="vh-100">
-        <div class="container py-5 h-100">
-          <div class="row d-flex align-items-center justify-content-center h-100">
-            <div class="col-md-8 col-lg-7 col-xl-6">
-              <img src={BannerLogin} class="img-fluid" alt="Login Banner"/>
-            </div>
-            <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
-              <form onSubmit={handleSubmit}>
-                <div class="form-outline mb-4">
-                  <label class="form-label" for="username">Username</label>
-                  <input type="text" 
-                    id="username" 
-                    class="form-control form-control-lg" 
-                    onChange={e => setUsername(e.target.value)}/>
-                </div>
+      <div class="row d-flex justify-content-center mt-4">
+        <img src={LogoIES} class="img-fluid w-25" alt="Logo IES" />
+        <h1 className='text-center pt-4'>Sistema de Controle de Presença</h1>
+      </div>
 
-                <div class="form-outline mb-4">
-                  <label class="form-label" for="password">Password</label>
-                  <input type="password"
-                    id="password" 
-                    class="form-control form-control-lg"
-                    onChange={e => setPassword(e.target.value)} />
-                </div>
+      <div class="container h-100 pt-4">
+        <div class="row d-flex align-items-center justify-content-center h-100">
+          <div class="col-md-8 col-lg-7 col-xl-6">
+            <img src={BannerLogin} class="img-fluid" alt="Login Banner" />
+          </div>
+          <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+            <form onSubmit={handleSubmit}>
+              <div class="form-outline mb-4">
+                <label class="form-label" for="username">Username</label>
+                <input type="text"
+                  id="username"
+                  class="form-control form-control-lg"
+                  onChange={e => setUsername(e.target.value)} />
+              </div>
 
-                <button type="submit" class="btn btn-primary btn-lg btn-block">Sign in</button>
-              </form>
-            </div>
+              <div class="form-outline mb-4">
+                <label class="form-label" for="password">Password</label>
+                <input type="password"
+                  id="password"
+                  class="form-control form-control-lg"
+                  onChange={e => setPassword(e.target.value)} />
+              </div>
+
+              <button type="submit" class="btn btn-lg btn-login">Sign in</button>
+            </form>
           </div>
         </div>
       </div>
+
     </>
   )
 }
